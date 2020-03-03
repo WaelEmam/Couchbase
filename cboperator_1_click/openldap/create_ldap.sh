7368d663bc7865a22cbae9ae0e4b8d4a1320e911
Wael <wael.emam@gmail.com>
2020-03-03 15:08:32 -0500

    create and destroy ldap

---
+++ cboperator_1_click/openldap/create_ldap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl run openldap --image=osixia/openldap:1.3.0 --port=389 --env LDAP_ORGANISATION="Couchbase" --env LDAP_DOMAIN="wael.couchbase.com" --env LDAP_ADMIN_PASSWORD="admin123"
+kubectl expose deployment openldap --type=LoadBalancer --port=389 --target-port=389
+sleep 40
+ldapadd -x -H ldap://localhost:389 -D "cn=admin,dc=wael,dc=couchbase,dc=com" -w admin123 -f ldap.ldif
