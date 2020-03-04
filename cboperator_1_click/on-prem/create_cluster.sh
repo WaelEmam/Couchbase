69fe085d0f0e4d5f88b7d13094887797cb3621a0
WaelEmam <wael.emam@gmail.com>
2020-03-04 15:53:44 -0500

    clean up

--- cboperator_1_click/on-prem/create_cluster.sh
+++ cboperator_1_click/on-prem/create_cluster.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 
-# Clean up
-
-#kubectl delete -f couchbase-cluster.yaml
-#kubectl delete -f secret.yaml
-#kubectl delete -f operator-role-binding.yaml
-#kubectl delete -f operator-role.yaml
-#kubectl delete -f operator-deployment.yaml
-#kubectl delete -f crd.yaml
-#kubectl delete -f admission.yaml
-#kubectl delete -f operator-service-account.yaml
-
 # Create CB Cluster
 
 kubectl create -f admission.yaml
@@ -23,15 +12,23 @@ kubectl create -f operator-deployment.yaml --namespace default
 kubectl create -f secret.yaml
 kubectl create -f couchbase-cluster.yaml
 
-#sleep 120
-#kubectl cp /Users/waelemam/mock_data/ecomm wael-cb-k8s-0000:/tmp/ecomm
+# Wait till all pods are up and running
+sleep 120
+
+kubectl cp /Users/waelemam/mock_data/ecomm wael-cb-k8s-0000:/tmp/ecomm
 kubectl port-forward wael-cb-k8s-0000 8091:8091 &
 
+
+# LDAP Integration
 ldap=`kubectl get pods -o wide | grep open| awk '{print $6}'`
 
 echo ${ldap}
 
 /Applications/Couchbase\ Server.app/Contents/Resources/couchbase-core/bin/couchbase-cli setting-ldap --cluster http://localhost --username Administrator --password password --hosts ${ldap} --port 389 --bind-dn 'cn=admin,dc=wael,dc=couchbase,dc=com' --bind-password 'admin123' --authentication-enabled 1 --user-dn-query 'ou=users,dc=wael,dc=couchbase,dc=com??one?(uid=%u)' --authorization-enabled 1 --group-query 'ou=groups,dc=wael,dc=couchbase,dc=com??one?(member=%D)'
+# /Applications/Couchbase\ Server.app/Contents/Resources/couchbase-core/bin/couchbase-cli setting-ldap --cluster http://localhost --username Administrator --password password --hosts ${ldap} --port 389 --bind-dn 'cn=admin,dc=wael,dc=couchbase,dc=com' --bind-password 'admin123' --authentication-enabled 1
+
+
+# kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/ecom_products.json -g key::%_id% -t 4"
 
 # cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/ecom_products.json -g key::%_id% -t 4
 # cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/ecom_reviews.json -g key::%_id% -t 4
