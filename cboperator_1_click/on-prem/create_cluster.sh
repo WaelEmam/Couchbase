9b71e51fca6a614d8334a7ce4ce9dd2ddef0843f
Wael <wael.emam@gmail.com>
2020-03-04 11:30:55 -0500

    create ldap structure

--- cboperator_1_click/on-prem/create_cluster.sh
+++ cboperator_1_click/on-prem/create_cluster.sh
@@ -23,9 +23,15 @@ kubectl create -f operator-deployment.yaml --namespace default
 kubectl create -f secret.yaml
 kubectl create -f couchbase-cluster.yaml
 
-sleep 120
+#sleep 120
 #kubectl cp /Users/waelemam/mock_data/ecomm wael-cb-k8s-0000:/tmp/ecomm
-kubectl port-forward wael-cb-k8s-0000 8091:8091
+kubectl port-forward wael-cb-k8s-0000 8091:8091 &
+
+ldap=`kubectl get pods -o wide | grep open| awk '{print $6}'`
+
+echo ${ldap}
+
+/Applications/Couchbase\ Server.app/Contents/Resources/couchbase-core/bin/couchbase-cli setting-ldap --cluster http://localhost --username Administrator --password password --hosts ${ldap} --port 389 --bind-dn 'cn=admin,dc=wael,dc=couchbase,dc=com' --bind-password 'admin123' --authentication-enabled 1 --user-dn-query 'ou=users,dc=wael,dc=couchbase,dc=com??one?(uid=%u)' --authorization-enabled 1 --group-query 'ou=groups,dc=wael,dc=couchbase,dc=com??one?(member=%D)'
 
 # cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/ecom_products.json -g key::%_id% -t 4
 # cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/ecom_reviews.json -g key::%_id% -t 4
