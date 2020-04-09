924bc5f7b9d5308c007e82398fcf516131cd01c1
WaelEmam <wael.emam@gmail.com>
2020-04-09 15:50:30 -0400

    restructure directories

--- cboperator_1_click/on-prem/clean_up.sh
+++ cboperator_1_click/on-prem/clean_up.sh
@@ -7,13 +7,13 @@ port_forward=`ps -ef | grep 8091| grep -v grep| awk '{print $2}'`
 kill -9 ${port_forward}
 
 kubectl delete -f couchbase-cluster.yaml
-kubectl delete -f secret.yaml
-kubectl delete -f operator-role-binding.yaml
-kubectl delete -f operator-role.yaml
-kubectl delete -f operator-deployment.yaml
-kubectl delete -f crd.yaml
-kubectl delete -f admission.yaml
-kubectl delete -f operator-service-account.yaml
+kubectl delete -f operator_files/secret.yaml
+kubectl delete -f operator_files/operator-role-binding.yaml
+kubectl delete -f operator_files/operator-role.yaml
+kubectl delete -f operator_files/operator-deployment.yaml
+kubectl delete -f operator_files/crd.yaml
+kubectl delete -f operator_files/admission.yaml
+kubectl delete -f operator_files/operator-service-account.yaml
 #kubectl delete -f couchmart.yml
 kubectl delete -f couchmart/couchbase-cluster-1.yaml
 kubectl delete -f couchmart/couchbase-cluster-2.yaml
