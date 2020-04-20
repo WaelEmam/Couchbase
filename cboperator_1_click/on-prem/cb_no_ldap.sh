a91a53703e3dbc5d3047b65027b54e0bf952b5e9
WaelEmam <wael.emam@gmail.com>
2020-04-20 15:39:20 -0400

    Allow user to enter CB cluster name

--- cboperator_1_click/on-prem/cb_no_ldap.sh
+++ cboperator_1_click/on-prem/cb_no_ldap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+echo "Enter Cluster Name"
+read cluster
+
 # Create CB Cluster
 
 kubectl create -f operator_files/admission.yaml
@@ -9,37 +13,38 @@ kubectl create -f operator_files/operator-service-account.yaml --namespace defau
 kubectl create -f operator_files/operator-role-binding.yaml --namespace default
 kubectl create -f operator_files/operator-deployment.yaml --namespace default
 kubectl create -f operator_files/secret.yaml
-kubectl create -f couchbase-cluster.yaml
+yq w templates/couchbase-cluster_temp.yaml "metadata.name" "${cluster}" > couchbase-cluster_${cluster}.yaml
+kubectl create -f couchbase-cluster_${cluster}.yaml
 
 # Wait till all pods are up and running
 echo " "
 echo "Waiting for Pods to start"
 sleep 120
-kubectl port-forward wael-cb-k8s-0000 8091:8091 &
+kubectl port-forward ${cluster}-0000 8091:8091 &
 
 
 # Copy Ecomm Data Set (50 Users, 100 Products, 60 Orders, 30 Reviews)
-kubectl cp data/ecomm wael-cb-k8s-0000:/tmp/ecomm
+kubectl cp data/ecomm ${cluster}-0000:/tmp/ecomm
 
 # Copy Music Data Set (10 Countries, 50 Users, 50 Tracks, 50 Playlists)
-kubectl cp data/music wael-cb-k8s-0000:/tmp/music
+kubectl cp data/music ${cluster}-0000:/tmp/music
 
 # Copy Contacts Data Set (50 Contacts)
-kubectl cp data/contacts wael-cb-k8s-0000:/tmp/contacts
+kubectl cp data/contacts ${cluster}-0000:/tmp/contacts
 
 # Import Data set to ecommerce bucket
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/reviews.json -g key::%_id% -t 4"
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/users.json -g key::%_id% -t 4"
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/products.json -g key::%_id% -t 4"
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/orders.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/reviews.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/users.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/products.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b ecommerce -f list -d file:///tmp/ecomm/orders.json -g key::%_id% -t 4"
 
 # Import Music Data Set
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/countries.json -g key::%_id% -t 4"
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/users.json -g key::%_id% -t 4"
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/tracks.json -g key::%_id% -t 4"
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/playlists.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/countries.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/users.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/tracks.json -g key::%_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b music -f list -d file:///tmp/music/playlists.json -g key::%_id% -t 4"
 
 
 # Import Contacts Dataset
-kubectl exec wael-cb-k8s-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b contacts -f list -d file:///tmp/contacts/contacts.json -g key::%contact_id% -t 4"
+kubectl exec ${cluster}-0000 -- bash -c "cbimport json -c couchbase://localhost -u Administrator -p password -b contacts -f list -d file:///tmp/contacts/contacts.json -g key::%contact_id% -t 4"
 
